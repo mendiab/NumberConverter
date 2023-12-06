7ca184f3e662a8502c0f4d922432e50ccc45ba6e
mendiab <men.diab@t-online.de>
2023-12-06 22:13:03 +0100

    first commit

---
+++ src/test/java/de/menouer/numberconverter/NumberConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.menouer.numberconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
